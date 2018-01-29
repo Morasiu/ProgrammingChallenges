9c13b40869d8f5ad73b27a1f8ea24884fd1bc09b
Morasiu <moras2601@wp.pl>
2018-01-29 13:10:33 +0100

    Name generator

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ All task from list of Programming Challenges v1.4
 ![list](docs/list.png)
 
 Progress:
-* 00 - pending
+* 00 Name Generator - Done (Python 3)
+* 01 Higher/lower - Pending
---
+++ challenges/00_Name_Generator/name_generator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys, random
+
+print("\n<-------_Name generator 0.1.0--------->")
+print("Created by Morasiu (morasiu2@gmail.com)\n")
+try:
+    lengh = int(raw_input("Enter name lengh: "))
+except:
+    print("Not a number")
+    sys.exit()
+
+consonants = "bcdfghjklmnqprstvwxz"
+vovels = "aeiouy"
+
+name = ""
+# Should name start with vovel.
+start_vovel = random.randint(0,1)
+
+
+for x in range(0, lengh):
+    if len(name) >lengh:
+        continue
+
+    if start_vovel:
+        if x%2==1:
+            name += random.choice(consonants) 
+            if random.randint(0,3) == 0:
+                name += random.choice(consonants) 
+        else:
+            name += random.choice(vovels)
+    else:
+        if x%2==1:
+            name += random.choice(vovels) 
+        else:
+            name += random.choice(consonants)
+            if random.randint(0,3) == 0:
+                name += random.choice(consonants)
+
+
+print("Your name:\n"+ name)
