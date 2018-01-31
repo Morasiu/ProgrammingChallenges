130cffd59df04461671e4fe6a0a86efc3c0256c3
Morasiu <moras2601@wp.pl>
2018-01-31 10:57:29 +0100

    Done Higher/Lower

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ All task from list of Programming Challenges v1.4
 Progress:
 * 00 Name Generator - 29.01.2018 *Done* (`Python 3`) <br>
 ![00](docs/00.png)
-* 01 Higher/lower - Pending
+* 01 Higher/lower - 31.01.2018 *Done* (C#) <br>
+![01](docs/01.png)
--- challenges/01_Higher_Lower/HigherLower.cs
+++ challenges/01_Higher_Lower/HigherLower.cs
@@ -6,18 +6,72 @@ class HigherLower{
     Console.WriteLine("Created by Morasiu (morasiu2@gmail.com)");
     Console.Write("Press any key to start.");
     Console.ReadKey();
-    Console.WriteLine("Game Start!");
-    GetDifficultLevel();
-
+    
+    StartGame();
   }
 
-  static int  GetDifficultLevel(){
-    Console.WriteLine("1. Easy (1-10)\n2. Medium (1-100)\n3. Hard (1-1000)\nPick difficult level: ");
+  static void StartGame(){
+    int maxNumber = GetDifficultLevel();
+    Random rand = new Random();
+    //First number is included, but second is exluded so I needed to add 1.
+    int randomNumber  = rand.Next(1, maxNumber + 1);
+    int number = 0;
+    int attempt = 0;
+    Console.WriteLine("\nGame Start!");
+
+    //Start guessing loop.
+    do {
+      Console.Write("Enter valid number: ");
+      //Try parse string from Console to int.
+      try {
+        number = int.Parse(Console.ReadLine());
+      } catch {
+        Console.WriteLine("Not a valid number.");
+        continue;
+      }
+      if(number == randomNumber){
+        attempt++;
+        Console.WriteLine("Hurray! You won :) Attempts: " + attempt.ToString());
+      } else if(number > randomNumber){
+        attempt++;
+        Console.WriteLine("Lower");
+      } else if (number < randomNumber){
+        attempt++;
+        Console.WriteLine("Higher");
+      }
+    } while(number != randomNumber);
+    //Ask if player want to play again.
+    PlayAgain(); 
+  }
+
+  static void PlayAgain(){
+    Console.Write("Do you want to play again? (y/n): ");
+    string decision = Console.ReadLine();
+    if (decision == "y")
+      StartGame();
+    else if (decision == "n"){
+      Console.WriteLine("Thanks for playing. :)");
+    } else {
+      Console.WriteLine("Wrong option");
+      PlayAgain();
+    }
+  }
+
+  static int GetDifficultLevel(){
+    Console.WriteLine("\n1. Easy (1-10)\n2. Medium (1-100)\n3. Hard (1-1000)\nPick difficult level: ");
     int maxNumber = 0;
     var difficult = Console.ReadLine();
-    if (difficult = "1"){                        
-    
+    if (difficult == "1")                        
+      maxNumber = 10;
+    else if (difficult == "2")
+      maxNumber = 100;
+    else if(difficult == "3")
+      maxNumber = 1000;
+    else {
+      Console.WriteLine("Wrong option.");
+      maxNumber = GetDifficultLevel();
     }
-    return difficult;
+
+    return maxNumber;
   }
 }
